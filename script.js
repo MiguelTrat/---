d47deb284bdae64bcc746473f26c8139c4291b66
migueltrat <105807283+MiguelTrat@users.noreply.github.com>
2023-02-10 20:27:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+ jQuery(document).ready(function(){
+  $('.header__link-one').mousemove(function(e){
+    var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+    var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+    $('.header__link-one').css('text-shadow', +rYP/10+'px '+rXP/80+'px #a0f016, '+rYP/8+'px '+rXP/60+'px  #527514, '+rXP/70+'px '+rYP/12+'px #253803');
+  });
+});
+
+jQuery(document).ready(function(){
+  $('.header__link-two').mousemove(function(e){
+    var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+    var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+    $('.header__link-two').css('text-shadow', +rYP/10+'px '+rXP/80+'px #a0f016, '+rYP/8+'px '+rXP/60+'px  #527514, '+rXP/70+'px '+rYP/12+'px #253803');
+  });
+});
+
+jQuery(document).ready(function(){
+  $('.header__link-three').mousemove(function(e){
+    var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+    var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+    $('.header__link-three').css('text-shadow', +rYP/10+'px '+rXP/80+'px #a0f016, '+rYP/8+'px '+rXP/60+'px  #527514, '+rXP/70+'px '+rYP/12+'px #253803');
+  });
+});
+
+jQuery(document).ready(function(){
+  $('.header__link-four').mousemove(function(e){
+    var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+    var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+    $('.header__link-four').css('text-shadow', +rYP/10+'px '+rXP/80+'px #a0f016, '+rYP/8+'px '+rXP/60+'px  #527514, '+rXP/70+'px '+rYP/12+'px #253803');
+  });
+});
+
+jQuery(document).ready(function(){
+  $('.header__link-five').mousemove(function(e){
+    var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+    var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+    $('.header__link-five').css('text-shadow', +rYP/10+'px '+rXP/80+'px #a0f016, '+rYP/8+'px '+rXP/60+'px  #527514, '+rXP/70+'px '+rYP/12+'px #253803');
+  });
+});
+ 
+
+ 
